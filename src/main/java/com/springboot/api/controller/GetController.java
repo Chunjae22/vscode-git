a24f2bf135f04359fd16d5e0b54a5e55e310fca5
GimHayeong-eng <hychunjae2021@gmail.com>
2023-02-24 14:36:50 +0900

    GetController

---
+++ src/main/java/com/springboot/api/controller/GetController.java
@@ -0,0 +1,46 @@
+package com.springboot.api.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class GetController {
+
+    @RequestMapping(value="/hello", method=RequestMethod.GET)
+    public String getHello() {
+        return "Hello World!!";
+    }
+
+    @RequestMapping(value="/name", method=RequestMethod.GET)
+    public String getName() {
+        return "Flautre";
+    }
+
+    @GetMapping(value="/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable) {
+        return variable;
+    }
+
+    @GetMapping(value="/request1")
+    public String getRequestParam1(@RequestParam String name, @RequestParam String email, @RequestParam String organization) {
+        return name + " " + email + " " + organization;
+    }
+
+    @GetMapping(value="/request2")
+    public String getRequestParam2(@RequestParam Map<String, String> param){
+        StringBuilder sb = new StringBuilder();
+        param.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue());
+        });
+
+        return sb.toString();
+    }
+}
